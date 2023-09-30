3219be96f30d0ea2bef5a200fc963defdc3947fd
tranphat1506 <chouphat1506@gmail.com>
2023-09-30 11:44:41 +0700

    change color movie detail

--- src/pages/MovieDetail/Page.tsx
+++ src/pages/MovieDetail/Page.tsx
@@ -29,7 +29,7 @@ const MovieDetailPage: React.FC<MovieDetailPageProps> = ({
                         {/* Rating in header */}
                         <span className="my-2 gap-8 md:flex hidden">
                             <span className="inline-flex flex-col items-center">
-                                <div className="uppercase font-MP_Bold text-base dark:text-[#ffffffb2] text-red-500 w-max">
+                                <div className="uppercase font-MP_Bold text-base dark:text-[#B8B8B8] text-red-500 w-max">
                                     HCine Rating
                                 </div>
                                 <span className="inline-flex items-center gap-2 cursor-pointer hover:bg-[#ffffff1e] px-2 py-[1px] rounded">
@@ -38,7 +38,7 @@ const MovieDetailPage: React.FC<MovieDetailPageProps> = ({
                                         <span className="text-xl font-MP_Bold">
                                             5.2
                                         </span>
-                                        <span className="text-base dark:text-[#ffffffb2] text-[#6a6a6a]">
+                                        <span className="text-base dark:text-[#B8B8B8] text-[#6a6a6a]">
                                             /10
                                         </span>
                                     </span>
@@ -77,48 +77,52 @@ const MovieDetailPage: React.FC<MovieDetailPageProps> = ({
                         </div>
                         {/* Detail */}
                         <div className="flex mx-6 flex-col font-MP_Medium w-full">
-                            <span className="flex gap-5 items-center">
+                            <span className="!mb-2 dark:!text-[#ffffffb2]">
+                                {['Kinh dị', 'Drama'].map((name, index) => {
+                                    return (
+                                        <Link
+                                            key={index}
+                                            to={''}
+                                            className="font-MP_Medium text-base px-2 py-1 border-[1px] rounded-full mr-4 hover:bg-[#ffffff17]"
+                                        >
+                                            {name}
+                                        </Link>
+                                    );
+                                })}
+                            </span>
+                            <span className="flex gap-5 items-center my-3">
                                 <span className="font-MP_Medium text-base flex-col inline-flex items-center">
-                                    <IoEarth />
+                                    <IoEarth className="text-xl" />
                                     Việt Nam
                                 </span>
                                 <span className="font-MP_Medium text-base flex-col inline-flex items-center">
-                                    <AiOutlineClockCircle />
+                                    <AiOutlineClockCircle className="text-xl" />
                                     1h35m
                                 </span>
                                 <span className="font-MP_Medium text-base flex-col inline-flex items-center">
-                                    <BiMoviePlay />
+                                    <BiMoviePlay className="text-xl" />
                                     Không phụ đề
                                 </span>
                             </span>
-                            <Breadcrumbs
-                                separator={<RxDotFilled />}
-                                aria-label="type_of_movie"
-                                className="!mb-2 dark:!text-[#ffffffb2]"
-                            >
-                                <Link
-                                    to={''}
-                                    className="font-MP_Medium text-base hover:underline"
+                            <span className="mb-2 dark:bg-[#ffffff0c] bg-[#ff7c7c03] px-6 py-2 pt-4">
+                                <span className="line-clamp-3 rounded dark:text-[#cecece] text-[#141414]">
+                                    Mười Chín Sang Hai Mươi - 19/20 là chương
+                                    trình sẽ theo sau những người thuộc thế hệ Z
+                                    khi họ tận hưởng tuần cuối cùng ở tuổi 19 và
+                                    tuần đầu tiên ở tuổi 20 sau khi đón giao năm
+                                    2023. Các thí sinh độc đáo sẽ kết thúc năm
+                                    bằng việc dành thời gian với bạn bè và từ từ
+                                    chuẩn bị cho tuổi trưởng thành.
+                                </span>
+                                <a
+                                    href={'#movie-desc'}
+                                    className="text-sm cursor-pointer float-right"
                                 >
-                                    Kinh dị
-                                </Link>
-                                <Link
-                                    to={''}
-                                    className="font-MP_Medium text-base hover:underline"
-                                >
-                                    Việt Nam
-                                </Link>
-                            </Breadcrumbs>
-                            <span className="mb-2 line-clamp-3">
-                                Mười Chín Sang Hai Mươi - 19/20 là chương trình
-                                sẽ theo sau những người thuộc thế hệ Z khi họ
-                                tận hưởng tuần cuối cùng ở tuổi 19 và tuần đầu
-                                tiên ở tuổi 20 sau khi đón giao năm 2023. Các
-                                thí sinh độc đáo sẽ kết thúc năm bằng việc dành
-                                thời gian với bạn bè và từ từ chuẩn bị cho tuổi
-                                trưởng thành.
+                                    Xem thêm
+                                </a>
                             </span>
-                            <span className="mb-2 gap-8 md:flex hidden">
+                            {/* Rating */}
+                            <span className="my-3 gap-8 md:flex hidden">
                                 <span className="inline-flex items-center gap-2 cursor-pointer hover:bg-[#ffffff1e] px-2 py-[1px] rounded">
                                     <BsStarFill className="text-[#ffce65] text-xl" />
                                     <span>
@@ -146,33 +150,33 @@ const MovieDetailPage: React.FC<MovieDetailPageProps> = ({
                                     </span> */}
                                 </span>
                             </span>
-                            <div className="flex flex-col font-MP_Regular text-base gap-1">
-                                <span>
-                                    <span className="text-[14px] uppercase">
+                            <div className="flex flex-col font-MP_Regular text-base">
+                                <span className="border-t-[1px] border-[#ffffff37] py-3">
+                                    <span className="uppercase text-base font-MP_Bold dark:text-[#cecece]">
                                         Ngôn ngữ:
                                     </span>
                                     <span className="font-MP_Medium mx-2">
                                         Tiếng Việt
                                     </span>
                                 </span>
-                                <span>
-                                    <span className="text-[14px] uppercase">
+                                <span className="border-t-[1px] border-[#ffffff37] py-3">
+                                    <span className="uppercase text-base font-MP_Bold dark:text-[#cecece]">
                                         Diễn viên
                                     </span>
                                     <span className="font-MP_Medium mx-2">
                                         Jason Statham, 50 Cent, Megan Fox
                                     </span>
                                 </span>
-                                <span>
-                                    <span className="text-[14px] uppercase">
+                                <span className="border-t-[1px] border-[#ffffff37] py-3">
+                                    <span className="uppercase text-base font-MP_Bold dark:text-[#cecece]">
                                         Đạo diễn:
                                     </span>
                                     <span className="font-MP_Medium mx-2">
                                         Scott Waugh
                                     </span>
                                 </span>
-                                <span>
-                                    <span className="text-[14px] uppercase">
+                                <span className="border-t-[1px] border-[#ffffff37] py-3">
+                                    <span className="uppercase text-base font-MP_Bold dark:text-[#cecece]">
                                         Ngày khởi chiếu:
                                     </span>
                                     <span className="font-MP_Medium mx-2">
@@ -181,12 +185,10 @@ const MovieDetailPage: React.FC<MovieDetailPageProps> = ({
                                 </span>
                             </div>
                         </div>
-                        {/* Rating */}
-                        <div className=""></div>
                     </div>
-                    <div className="mx-6 mt-4 font-MP_Regular">
+                    <div id="movie-desc" className="mx-6 pt-4 font-MP_Regular">
                         <div className="inline-flex h-8 items-center gap-2">
-                            <Divider className="!border-red-500 dark:!border-red-netflix !border-2 !h-full" />
+                            <Divider className="!border-red-500 dark:!border-red-netflix !border-4 !h-full" />
                             <span className="font-MP_Medium uppercase text-2xl">
                                 Nội dung phim
                             </span>
